d82b42610807a349101d23d56ffe559c304340c4
Ştefan-Alexandru DUMITRIU (110098) <stefan.dumitriu@stud.acs.upb.ro>
2020-11-07 19:39:46 +0200

    Added location permission and Location City Finder Feature

--- app/src/main/java/com/codeeina/app_covid/MyCountyStatsFragment.java
+++ app/src/main/java/com/codeeina/app_covid/MyCountyStatsFragment.java
@@ -1,5 +1,10 @@
 package com.codeeina.app_covid;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,10 +13,51 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MyCountyStatsFragment extends Fragment {
+    private FusedLocationProviderClient fusedLocationProviderClient;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+
+        if(getActivity().getApplicationContext().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if(location != null) {
+                        double latitude = location.getLatitude();
+                        double longitude = location.getLongitude();
+                        Geocoder gcd = new Geocoder(getContext(), Locale.getDefault());
+                        List<Address> addressList = null;
+                        try {
+                            addressList = gcd.getFromLocation(latitude, longitude, 1);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        if(addressList.size() > 0) {
+                            System.out.println(addressList.get(0).getLocality());
+                        }
+                        System.out.println(latitude + " " + longitude);
+                    }
+                }
+            });
+        }
+        else {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+
+
+
         return inflater.inflate(R.layout.fragment_my_county_stats, container, false);
     }
 }
